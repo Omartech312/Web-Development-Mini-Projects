04be690f733dcb72340a22099e1162ac926193aa
Omartech312 <sor22@nau.edu>
2026-08-02 15:11:56 -0700

    Simple style changes and comments

--- counter/script.js
+++ counter/script.js
@@ -1,9 +1,12 @@
+// Constants targeting -, reset, + buttons
 const reset = document.getElementById("reset");
 const add = document.getElementById("plus");
 const subtract = document.getElementById("minus");
 
+//counter displayed ont he website
 let count = 0;
 
+// When the buttons are clicked the counter gets ++, -- or reset back to 0 and the id="count" in HTML gets update
 reset.onclick = function(){
     count = 0;
     document.getElementById("count").textContent = count;
