8a33511902791c9bec847bdce3b9b253455cbb2a
Omartech312 <sor22@nau.edu>
2026-09-23 17:34:07 -0700

    page is now capable of adding objects to the array of products

--- display_products/script.js
+++ display_products/script.js
@@ -12,3 +12,18 @@ class Product{
         Product.productCount++;
     }
 }
+
+const ProductList = [];
+
+document.getElementById("Psubmit").addEventListener("click", () =>{
+    // retriving the provided data for display
+    let newProduct = new Product(document.getElementById("Pname").value,
+    document.getElementById("Pprice").value,
+    document.getElementById("Pquantity").value,
+    document.getElementById("Punit").value,
+    document.getElementById("Pimage").file)
+    
+    ProductList.push(newProduct);
+    console.log(ProductList);
+});
+
