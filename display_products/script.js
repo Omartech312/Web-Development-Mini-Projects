265ade47364f2a91beb1fb700934f341d4b3744c
Omartech312 <sor22@nau.edu>
2026-09-23 22:46:00 -0700

    Improving visuals

--- display_products/script.js
+++ display_products/script.js
@@ -38,7 +38,7 @@ function displayProduct(Object, index, array){
     document.getElementById("container").innerHTML += `
     <div class="product">
     <p>${Object.name}</p>
-    <button onclick="remove(${index})">X</button>
+    <button class="removeB" onclick="remove(${index})">X</button>
     <p>${Object.quantity} ${Object.unit}</p>
     ${imageURL ? `<img src="${imageURL}" class="productImage">` : ""} 
     <p>$${Object.price}</p>
