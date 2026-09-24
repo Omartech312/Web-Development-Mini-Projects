24a268eaf9848c957345d1c272cf09c0b398cec9
Omartech312 <sor22@nau.edu>
2026-09-23 18:24:13 -0700

    Products are now displaying on the page

--- display_products/script.js
+++ display_products/script.js
@@ -21,9 +21,27 @@ document.getElementById("Psubmit").addEventListener("click", () =>{
     document.getElementById("Pprice").value,
     document.getElementById("Pquantity").value,
     document.getElementById("Punit").value,
-    document.getElementById("Pimage").file)
+    document.getElementById("Pimage").files[0])
     
     ProductList.push(newProduct);
     console.log(ProductList);
+    document.getElementById("container").innerHTML = "";
+    ProductList.forEach(displayProduct);
 });
 
+
+function displayProduct(Object){
+    let imageURL = "";
+    if(Object.image){
+        imageURL = URL.createObjectURL(Object.image);
+    }
+    document.getElementById("container").innerHTML += `
+    <div class="product">
+    <p>${Object.name}</p>
+    <p>${Object.quantity} ${Object.unit}</p>
+    ${imageURL ? `<img src="${imageURL}" class="productImage">` : ""} 
+    <p>$${Object.price}</p>
+    
+    </div>
+    `;
+}
